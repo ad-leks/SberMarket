87b5cea05989664d92c3466ad1a5314b383582e5
LenOK2719 <lena2006-07@yandex.ru>
2020-04-06 21:37:40 +0300

    update

--- CheckDay.py
+++ CheckDay.py
@@ -1,27 +1,56 @@
 ## -*- coding: utf-8 -*-
 import requests
 from datetime import date, timedelta
+import telebot
+import os
 import json
 import csv
 import re
 import sys
 
-date_start = date.today()
-date_stop = date.today() + timedelta(6)
-date_start = date(date_start.year, date_start.month, date_start.day)
-date_stop = date(date_stop.year, date_stop.month, date_stop.day)
-lsDate = []
+def get_intervals(cart_id):
+	date_start = date.today()
+	date_stop = date.today() + timedelta(6)
+	date_start = date(date_start.year, date_start.month, date_start.day)
+	date_stop = date(date_stop.year, date_stop.month, date_stop.day)
+	slots = []
 
-url = 'https://sbermarket.ru/api/shipments/H02815115275/shipping_rates'
-paramsForUrl = dict()
+	url = 'https://sbermarket.ru/api/shipments/{0}/shipping_rates'.format(cart_id)
+	paramsForUrl = {}
 
-delta = date_stop - date_start
-if delta.days<=0:
-    print ("Фигня какая-то в датах")
-for i in range(delta.days + 1):
-	lsDate.append(date_start + timedelta(i))
+	delta = date_stop - date_start
+	if delta.days <= 0:
+		print("Фигня какая-то в датах")
+	for i in range(delta.days + 1):
+		slots.append({"id": i, "day": date_start + timedelta(i)})
 
-for day in lsDate:
-	paramsForUrl ["date"] = day
-	re = requests.get(url, params=paramsForUrl)
-	print(re.text)
+	print(slots)
+
+	for slot in slots:
+		paramsForUrl["date"] = slot.get("day")
+		r = requests.get(url, params=paramsForUrl).json()
+		intervals = []
+		for window in r.get("shipping_rates"):
+			if window.get("is_free") is True:
+				intervals.append(window.get("delivery_window").get("human_interval"))
+		slot.update({"intervals": intervals})
+
+	for slot in slots:
+		print(slot)
+
+	return slots
+
+def alert(slots):
+	token = os.environ("TOKEN")
+	bot = telebot.TeleBot(token)
+	print(bot.send_message(74152489, "test"))
+
+def main():
+	cart_id = "H42237155066"
+	slots = 1
+	#slots = get_intervals(cart_id)
+	alert(slots)
+
+if __name__ == '__main__':
+	main()
+
